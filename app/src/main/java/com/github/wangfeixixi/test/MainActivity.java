fbe5c817f6d14688711f38b6e5ce729b997bcdba
xuanyuanxixi <1020301980@qq.com>
2018-02-02 10:32:02 +0800

    3

--- app/src/main/java/com/github/wangfeixixi/test/MainActivity.java
+++ app/src/main/java/com/github/wangfeixixi/test/MainActivity.java
@@ -5,19 +5,4 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreatefaadsafasdfa(Bundle savedInstanceState) {
-        super.onCreate(saveds
-        fasdfasString d = "asfssf";        setContentView(R.layout.activity_main);
-        String a = "asfasf";
-        String b = "asfasf";
-        String casdfa"asfasf";f
-        String d = "asfasf";sd
-        String d = "asfasf";        asetContentView(R.layout.activity_main);
-        String a = "asfdfasdfasf";
-        String c = "asfasf";fas
-        String d = "asfasf";        setContentView(R.layout.activity_main);
-        Staasdfasdfasdfasdfsf";    fas    setContentView(R.layout.activity_main);
-        String a = "asfasf";
-        String b = "asfasf";
-        Sasdfasdfasdfa
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
