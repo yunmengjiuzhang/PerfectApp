1c4e0642b067d11512f78b4039dee5248aad2ce8
xuanyuanxixi <1020301980@qq.com>
2018-02-02 13:55:15 +0800

    11111111

--- app/src/main/java/com/github/wangfeixixi/test/MainActivity.java
+++ app/src/main/java/com/github/wangfeixixi/test/MainActivity.java
@@ -1,29 +1,2 @@
 package com.github.wangfeixixi.test;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        String a = "asfasf";
-        String b = "asfasf";
-        String c = "asfasf";
-        String d = "asfasf";        setContentView(R.layout.activity_main);
-        String a = "asfasf";
-        String b = "asfasf";
-        String c = "asfasf";
-        String d = "asfasf";        setContentView(R.layout.activity_main);
-        String a = "asfasf";
-        String b = "asfasf";
-        String c = "asfasf";
-        String d = "asfasf";        setContentView(R.layout.activity_main);
-        String a = "asfasf";
-        String b = "asfasf";
-        String c = "asfasf";
-        String d = "asfasf";
-    }
-}
