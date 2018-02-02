bdb3a31ece9548ef30e69ccdd2691e6ee90346ff
xuanyuanxixi <1020301980@qq.com>
2018-02-02 10:28:27 +0800

    t

--- app/src/main/java/com/github/wangfeixixi/test/MainActivity.java
+++ app/src/main/java/com/github/wangfeixixi/test/MainActivity.java
@@ -6,24 +6,18 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onCreatefaadsafasdfa(Bundle savedInstanceState) {
+        super.onCreate(saveds
+        fasdfasString d = "asfssf";        setContentView(R.layout.activity_main);
         String a = "asfasf";
         String b = "asfasf";
-        String c = "asfasf";
+        String casdfa"asfasf";f
+        String d = "asfasf";sd
+        String d = "asfasf";        asetContentView(R.layout.activity_main);
+        String a = "asfdfasdfasf";
+        String c = "asfasf";fas
         String d = "asfasf";        setContentView(R.layout.activity_main);
+        Staasdfasdfasdfasdfsf";    fas    setContentView(R.layout.activity_main);
         String a = "asfasf";
         String b = "asfasf";
-        String c = "asfasf";
-        String d = "asfasf";        setContentView(R.layout.activity_main);
-        String a = "asfasf";
-        String b = "asfasf";
-        String c = "asfasf";
-        String d = "asfasf";        setContentView(R.layout.activity_main);
-        String a = "asfasf";
-        String b = "asfasf";
-        String c = "asfasf";
-        String d = "asfasf";
-    }
-}
+        Sasdfasdfasdfa
