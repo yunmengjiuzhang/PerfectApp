7a82760a506014a67486a8555a55dcbc570c81b9
xuanyuanxixi <1020301980@qq.com>
2018-02-02 10:29:47 +0800

    m

--- app/src/main/java/com/github/wangfeixixi/test/MainActivity.java
+++ app/src/main/java/com/github/wangfeixixi/test/MainActivity.java
@@ -20,4 +20,89 @@ public class MainActivity extends AppCompatActivity {
         Staasdfasdfasdfasdfsf";    fas    setContentView(R.layout.activity_main);
         String a = "asfasf";
         String b = "asfasf";
+        Sasdfasdfasdfapublic class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreatefaadsafasdfa(Bundle savedInstanceState) {
+        super.onCreate(saveds
+        fasdfasString d = "asfssf";        setContentView(R.layout.activity_main);
+        String a = "asfasf";
+        String b = "asfasf";
+        String casdfa"asfasf";f
+        String d = "asfasf";sd
+        String d = "asfasf";        asetContentView(R.layout.activity_main);
+        String a = "asfdfasdfasf";
+        String c = "asfasf";fas
+        String d = "asfasf";        setContentView(R.layout.activity_main);
+        Staasdfasdfasdfasdfsf";    fas    setContentView(R.layout.activity_main);
+        String a = "asfasf";
+        String b = "asfasf";
+        Sasdfasdfasdfapublic class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreatefaadsafasdfa(Bundle savedInstanceState) {
+        super.onCreate(saveds
+        fasdfasString d = "asfssf";        setContentView(R.layout.activity_main);
+        String a = "asfasf";
+        String b = "asfasf";
+        String casdfa"asfasf";f
+        String d = "asfasf";sd
+        String d = "asfasf";        asetContentView(R.layout.activity_main);
+        String a = "asfdfasdfasf";
+        String c = "asfasf";fas
+        String d = "asfasf";        setContentView(R.layout.activity_main);
+        Staasdfasdfasdfasdfsf";    fas    setContentView(R.layout.activity_main);
+        String a = "asfasf";
+        String b = "asfasf";
+        Sasdfasdfasdfapublic class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreatefaadsafasdfa(Bundle savedInstanceState) {
+        super.onCreate(saveds
+        fasdfasString d = "asfssf";        setContentView(R.layout.activity_main);
+        String a = "asfasf";
+        String b = "asfasf";
+        String casdfa"asfasf";f
+        String d = "asfasf";sd
+        String d = "asfasf";        asetContentView(R.layout.activity_main);
+        String a = "asfdfasdfasf";
+        String c = "asfasf";fas
+        String d = "asfasf";        setContentView(R.layout.activity_main);
+        Staasdfasdfasdfasdfsf";    fas    setContentView(R.layout.activity_main);
+        String a = "asfasf";
+        String b = "asfasf";
+        Sasdfasdfasdfapublic class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreatefaadsafasdfa(Bundle savedInstanceState) {
+        super.onCreate(saveds
+        fasdfasString d = "asfssf";        setContentView(R.layout.activity_main);
+        String a = "asfasf";
+        String b = "asfasf";
+        String casdfa"asfasf";f
+        String d = "asfasf";sd
+        String d = "asfasf";        asetContentView(R.layout.activity_main);
+        String a = "asfdfasdfasf";
+        String c = "asfasf";fas
+        String d = "asfasf";        setContentView(R.layout.activity_main);
+        Staasdfasdfasdfasdfsf";    fas    setContentView(R.layout.activity_main);
+        String a = "asfasf";
+        String b = "asfasf";
+        Sasdfasdfasdfapublic class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreatefaadsafasdfa(Bundle savedInstanceState) {
+        super.onCreate(saveds
+        fasdfasString d = "asfssf";        setContentView(R.layout.activity_main);
+        String a = "asfasf";
+        String b = "asfasf";
+        String casdfa"asfasf";f
+        String d = "asfasf";sd
+        String d = "asfasf";        asetContentView(R.layout.activity_main);
+        String a = "asfdfasdfasf";
+        String c = "asfasf";fas
+        String d = "asfasf";        setContentView(R.layout.activity_main);
+        Staasdfasdfasdfasdfsf";    fas    setContentView(R.layout.activity_main);
+        String a = "asfasf";
+        String b = "asfasf";
         Sasdfasdfasdfa
